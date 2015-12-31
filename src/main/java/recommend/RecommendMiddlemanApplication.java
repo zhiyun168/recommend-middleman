4e80409606dc4459bfc2bbfc5fd7c9e07e3ddb6f
ouduobiao <ouduobiao@gmail.com>
2015-12-31 17:04:16 +0800

    s3log

--- src/main/java/recommend/RecommendMiddlemanApplication.java
+++ src/main/java/recommend/RecommendMiddlemanApplication.java
@@ -1,12 +1,11 @@
 package recommend;
 
-import com.zhiyun168.service.api.recommend.IHealthNoticeService;
+import com.zhiyun168.service.api.recommend.detail.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
 
 
 import java.util.Arrays;
@@ -29,37 +28,7 @@ public class RecommendMiddlemanApplication {
         for (String beanName : beanNames) {
             log.info(beanName);
         }
-
-
-
-
-        //IUserRecommender rec = (IUserRecommender)ctx.getBean("rec");
-        //System.out.println(rec.getRandomCandidates(1082l,3));
-        //System.out.println(rec.getRandomCandidates(1082l,3));
-
-        //GoalRecommender goalRecommender = ctx.getBean(GoalRecommender.class);
-        //System.out.println(goalRecommender.getCandidates(1082l,3));
-        //System.out.println(goalRecommender.getCandidates(1082l,3));
-
-        //IGoalService goalService = ctx.getBean(IGoalService.class);
-
-        //System.out.println(goalService.isJoinGoal(13l,10l));
-
-        //TagRecommender tagRecommender = ctx.getBean(TagRecommender.class);
-        //System.out.println(tagRecommender.getCandidates(15204l, 1, 2));
-        //System.out.println(tagRecommender.getCandidates(66377l, 1, 2));
-
-
-        /*
-        //监听索引更新
-        Redisson redisson = ctx.getBean(Redisson.class);
-        UpdateRecMessageListener listener = ctx.getBean(UpdateRecMessageListener.class);
-        RTopic<String> topic =redisson.getTopic("recommendation");
-        topic.addListener(listener);
-        */
-        IHealthNoticeService healthNoticeService = ctx.getBean("ab", IHealthNoticeService.class);
-        System.out.println(healthNoticeService.getNoticeById(1088L, "GMT+8"));
-
+        
         log.info("recommend middleman start!");
     }
 }
