a60dbac23a51954750aa91afff3398a75957abe5
oudb <ouduobiao@gmail.com>
2016-11-09 17:52:29 +0800

    搜索打卡推荐 默认 new_user

--- src/main/java/recommend/service/recommender/v2/impl/SearchGoalRecommender.java
+++ src/main/java/recommend/service/recommender/v2/impl/SearchGoalRecommender.java
@@ -20,36 +20,14 @@ public class SearchGoalRecommender extends PagingRecommender implements ISearchG
 
     private static Logger log = LoggerFactory.getLogger(SearchGoalRecommender.class);
 
-    private static String defaultCandidatesStr = "46858:122500.460000 51357:12283.010000 51203:3289.000000 51083:3289.000000 51096:1293.020000 51692:748.000000 49284:693.010000 51542:457.800000 51697:207.700000 51655:0.000000 51159:0.000000";
 
-    private List<String> defaultCandidates;
-
-    public SearchGoalRecommender() {
-        try {
-            defaultCandidates = new ArrayList<>();
-            Splitter scoreSplitter = Splitter.on(":").omitEmptyStrings().trimResults();
-            for(String  itemAndScore: Splitter.on(" ").omitEmptyStrings().trimResults().split(defaultCandidatesStr) )
-            {
-                List<String> r = scoreSplitter.splitToList(itemAndScore);
-                defaultCandidates.add(r.get(0));
-            }
-        }
-        catch (Exception e)
-        {
-            log.error("初始化默认推荐列表失败");
-        }
-    }
 
     @Override
     public List<String> getCandidates(String uid, int page, int pageSize) {
         List<String> res =  getCandidates_(uid, page, pageSize);
         if(CollectionUtils.isEmpty(res))
         {
-            List<List<String>> par = Lists.partition(defaultCandidates, pageSize);
-            if(page <= par.size())
-            {
-                res = par.get(page-1);
-            }
+            res = getCandidates_("new_user", page, pageSize);
         }
         return res;
     }
